981ed9c34910389afeb221edbb0cae998c7bcf80
Opher Shachar <ophers@users.noreply.github.com>
2012-11-20 19:18:17 +0000

    Fix copyright line Issue #CIJU-1 - Upload initial sources

--- ciju-1.3.x/src/org/ciju/client/ipp/IppURLConnectionImpl.java
+++ ciju-1.3.x/src/org/ciju/client/ipp/IppURLConnectionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Opher Shachar, Ladpc Ltd.
+ * Copyright (C) 2012 Opher Shachar
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
