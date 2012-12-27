407b73477e265783407618e65235a81c6e03c615
Opher Shachar <ophers@users.noreply.github.com>
2012-12-27 22:09:43 +0000

    Updated IppEncoding: added OpCode enumeration

--- ciju-1.3.x/src/org/ciju/client/ipp/IppEncoding.java
+++ ciju-1.3.x/src/org/ciju/client/ipp/IppEncoding.java
@@ -169,4 +169,101 @@ public class IppEncoding {
         }
     }
     
+    
+    public enum OpCode {
+        // 0x0001 is reserved
+        PRINT_JOB(0x0002),
+        PRINT_URI(0x0003),
+        VALIDATE_JOB(0x0004),
+        CREATE_JOB(0x0005),
+        SEND_DOCUMENT(0x0006),
+        SEND_URI(0x0007),
+        CANCEL_JOB(0x0008),
+        GET_JOB_ATTRIBUTES(0x0009),
+        GET_JOBS(0x000A),
+        GET_PRINTER_ATTRIBUTES(0x000B),
+        HOLD_JOB(0x000C),
+        RELEASE_JOB(0x000D),
+        RESTART_JOB(0x000E),
+        // 0x000F is reserved
+        PAUSE_PRINTER(0x0010),
+        RESUME_PRINTER(0x0011),
+        PURGE_JOBS(0x0012),
+        // From RFC3380: IPP/1.1 - Job and Printer Set Operations
+        SET_PRINTER_ATTRIBUTES(0x0013),
+        SET_JOB_ATTRIBUTES(0x0014),
+        GET_PRINTER_SUPPORTED_VALUES(0x0015),
+        // From RFC3995: IPP/1.1 - Event Notifications and Subscriptions
+        CREATE_PRINTER_SUBSCRIPTIONS(0x0016),
+        CREATE_JOB_SUBSCRIPTIONS(0x0017),
+        GET_SUBSCRIPTION_ATTRIBUTES(0x0018),
+        GET_SUBSCRIPTIONS(0x0019),
+        RENEW_SUBSCRIPTION(0x001A),
+        CANCEL_SUBSCRIPTION(0x001B),
+        // From RFC3996: IPP/1.1 - The 'ippget' Delivery Method for Event Notifications
+        GET_NOTIFICATIONS(0x001C),
+        // 0x001D-0x0021 are reserved
+        // From RFC3998: IPP/1.1 - Job and Printer Administrative Operations
+        ENABLE_PRINTER(0x0022),
+        DISABLE_PRINTER(0x0023),
+        PAUSE_PRINTER_AFTER_CURRENT_JOB(0x0024),
+        HOLD_NEW_JOBS(0x0025),
+        RELEASE_HELD_NEW_JOBS(0x0026),
+        DEACTIVATE_PRINTER(0x0027),
+        ACTIVATE_PRINTER(0x0028),
+        RESTART_PRINTER(0x0029),
+        SHUTDOWN_PRINTER(0x002A),
+        STARTUP_PRINTER(0x002B),
+        REPROCESS_JOB(0x002C),
+        CANCEL_CURRENT_JOB(0x002D),
+        SUSPEND_CURRENT_JOB(0x002E),
+        RESUME_JOB(0x002F),
+        PROMOTE_JOB(0x0030),
+        SCHEDULE_JOB_AFTER(0x0031),
+        // 0x0032 is reserved
+        // From PWG5100.5: IPP/2.2 - Document Object
+        CANCEL_DOCUMENT(0x0033),
+        GET_DOCUMENT_ATTRIBUTES(0x0034),
+        GET_DOCUMENTS(0x0035),
+        DELETE_DOCUMENT(0x0036),
+        SET_DOCUMENT_ATTRIBUTES(0x0037),
+        // From PWG5100.11: IPP/2.1 - Job and Printer Extensions - Set 2
+        CANCEL_JOBS(0x0038),
+        CANCEL_MY_JOBS(0x0039),
+        RESUBMIT_JOB(0x003A),
+        CLOSE_JOB(0x003B),
+        // From PWG5100.13: IPP/2.1 - Job and Printer Extensions – Set 3
+        IDENTIFY_PRINTER(0x003C),
+        VALIDATE_DOCUMENT(0x003D),
+        ;
+        
+        private final int value;
+
+        private OpCode(int value) {
+            this.value = value;
+        }
+        
+        public int getValue() {
+            return value;
+        }
+        
+        
+        private static final Map<Integer, OpCode> vmap = initVmap();
+        private static Map<Integer, OpCode> initVmap() {
+            final OpCode[] ocs = OpCode.values();
+            final HashMap<Integer, OpCode> vmap = new HashMap<Integer, OpCode>(ocs.length);
+            for (OpCode e : ocs)
+                vmap.put(e.value, e);
+            return vmap;
+        }
+        
+        public static OpCode valueOf(Integer i) {
+            OpCode e = vmap.get(i);
+            if (i == null)
+                throw new NullPointerException("i is null");
+            if (e == null)
+                throw new IllegalArgumentException(String.format("No enum const has value 0x%02X", i));
+            return e;
+        }
+    }
 }
