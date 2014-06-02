85c4641836f09e7a307e84edb1cae39f8e08ac41
ophers <ophers@1f1dac2c-0c01-43e2-d6cc-950ea5bc2811>
2014-06-02 05:08:38 +0000

    evolved writeIppRequest(), writeRequest()

---
+++ ciju-1.3.x/src/javax/print/attribute/AttributesImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 Opher Shachar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package javax.print.attribute;
+
+import org.ciju.ipp.IppEncoding.ValueTag;
+
+/**
+ *
+ * @author Opher Shachar
+ */
+public class AttributesImpl {
+    
+    public static ValueTag deduceEnumIPPSyntax(EnumSyntax o) {
+        int i = o.getOffset();
+        int n = o.getEnumValueTable().length;
+        if (i > 0)
+            return ValueTag.ENUM;
+        if (n == 2)
+            return ValueTag.BOOLEAN;
+        return ValueTag.KEYWORD;
+    }
+
+}
